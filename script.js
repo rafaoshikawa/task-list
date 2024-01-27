92179c98c0aacb5816a04d79d3e6e20ec6607843
rafaoshikawa <rafael.oshikawa@hotmail.com>
2024-01-27 20:06:24 -0300

    modificações

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-$(document).ready(function () {
-  var taskForm = $("#task-form");
-  var taskList = $("#task-list");
+$(document).ready(function () { //todo o conteudo do jquery é importante estar tudo aqui dentro, ele só vai carregar o Jquery se todo o document estiver pronto
+  var taskForm = $("#task-form");//coletando os valores do form
+  var taskList = $("#task-list");//coletando valor do ul
 
   taskForm.submit(function (event) {
-    event.preventDefault();
+    event.preventDefault();//funçao de retirar o reload da pagina apos o submit 
 
-    var taskName = $("#task").val();
+    var taskName = $("#task").val();//recupera o valor do input 
 
-    if (taskName.trim() !== "") {
-      taskList.append("<li>" + taskName + "</li>");
-      $("#task").val("");
-    }
+    // Adiciona uma nova tarefa à lista, mesmo se o campo estiver vazio
+    taskList.append("<li>" + taskName + "</li>");
+
+    // Limpa o campo de input
+    $("#task").val("");
   });
 
   taskList.on("click", "li", function () {
